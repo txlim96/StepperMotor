728d7f9d6fdc5883dc697d88688bd5f4f25b2c1d
txlim96 <30485660+txlim96@users.noreply.github.com>
2020-02-25 01:35:08 +0800

    StepperMotor rev2.0
    - Uses kinematics equation to compute current expected velocity, then compute delay required

--- Stepper_Motor.cpp
+++ Stepper_Motor.cpp
@@ -19,22 +19,23 @@ StepperMotor::StepperMotor(uint8_t dir, uint8_t step, uint8_t en) {
 }
 
 void StepperMotor::setMaxSpeed(unsigned int rpm) {
-  _maxSpeed = 1000000 / (rpm * _STEPPER_CONST);
+  _maxSpeed = 1000000 * _STEPPER_CONST / rpm;
 }
 
-void StepperMotor::setAcceleration(unsigned int f, unsigned int d) {
+void StepperMotor::setAcceleration(unsigned int a) {
+  _acceleration.acceleration = a;
+  _lowBound = pow((1000000 * _STEPPER_CONST / _maxSpeed),2) / (2*a); // v^2 = u^2 + 2*a*s
   _acceleration.mode = _ACCELERATE;
-  _acceleration.repeatSpeed = f;
-  _acceleration.duration    = d;
+
+  _u = 0;
 }
 
 void StepperMotor::move(long relative) {
   _stepsToMove = abs(relative);
   _stepCount = 0;
   _endPos += relative;
-  
-  _lowBound = _acceleration.repeatSpeed*_acceleration.duration;
-  _upBound  = _stepsToMove - _lowBound;
+
+  _u = 0;
   _direction = (relative > 0 ? HIGH : LOW);
   digitalWrite(_pins[_DIR], _direction);
 }
@@ -44,8 +45,6 @@ void StepperMotor::moveTo(long absolute) {
   _stepCount = 0;
   _endPos = absolute;
 
-  _lowBound = _acceleration.repeatSpeed*_acceleration.duration;
-  _upBound  = _stepsToMove - _lowBound;
   _direction = (absolute > 0 ? HIGH : LOW);
   digitalWrite(_pins[_DIR], _direction);
 }
@@ -65,24 +64,26 @@ bool StepperMotor::runStep() {
     return false;
   }
 
-  unsigned int _stepInterval = 0;
-  
   digitalWrite(_pins[_EN], LOW);
-  if (_acceleration.mode == _ACCELERATE) {
-    if (_lowBound <= _stepCount && _stepCount <= _upBound) {
-      _stepInterval = _maxSpeed;
-    }
-    else {
-      unsigned int _accCount = (_stepCount > _upBound ? _stepsToMove-_stepCount : _stepCount);
-      _stepInterval = _maxSpeed * floor(_acceleration.duration-_accCount/_acceleration.repeatSpeed);
-    }
-  }
-  else _stepInterval = _maxSpeed;
-  
   unsigned long t = micros();
   if (t - _lastStepTime >= _stepInterval) {
+    if (_acceleration.mode == _ACCELERATE) {
+      if (_lowBound < _stepCount && _stepCount < _stepsToMove-_lowBound) {
+        _stepInterval = _maxSpeed;
+      }
+      else {
+        int d = _stepCount < _lowBound ? 1 : -1;
+        _v = sqrt(pow(_u,2) + 2*_acceleration.acceleration*d);
+        _u = _v;
+        _stepInterval = 1000000 * _STEPPER_CONST / _v;
+      }
+    }
+    else _stepInterval = _maxSpeed;
+    
     _currPos += (_direction ? 1 : -1);
     _stepCount += step();
+//    Serial.println(_stepInterval);
+    
     _lastStepTime = t;
     return true;
   }
--- Stepper_Motor.h
+++ Stepper_Motor.h
@@ -3,7 +3,7 @@
 
 #include "Arduino.h"
 
-#define _STEPPER_CONST 6/1.8  // 360/(step_angle*60s)
+#define _STEPPER_CONST 1.8/6  // (step_angle*60s)/360
 
 #define _DIR  0
 #define _STEP 1
@@ -16,7 +16,7 @@ class StepperMotor {
   public:
     StepperMotor(uint8_t dir, uint8_t step, uint8_t en);
     void setMaxSpeed(unsigned int rpm);
-    void setAcceleration(unsigned int, unsigned int);
+    void setAcceleration(unsigned int);
     void move(long);
     void moveTo(long);
     void setCurrentPosition(long);
@@ -27,20 +27,22 @@ class StepperMotor {
     uint8_t _pins[3]  = {2, 3, 4};
     
     bool  _direction  = true;
-    unsigned int  _maxSpeed   = 0;
+    unsigned int  _maxSpeed     = 0;
+    unsigned int  _stepInterval = 0;
     unsigned long _stepCount  = 0;
     unsigned long _stepsToMove  = 0;
     unsigned long _lastStepTime = 0;
 
     long  _currPos  = 0;
     long  _endPos   = 0;
+    unsigned int  _lowBound = 0;
+    float _u = 0;
+    float _v = 0;
     unsigned long _upBound  = 0;
-    unsigned long _lowBound = 0;
 
     typedef struct {
       bool  mode = _NORMAL;
-      uint8_t repeatSpeed = 20;
-      uint8_t duration    = 5;
+      int acceleration = 0;
     } Acceleration;
     Acceleration _acceleration;
 
